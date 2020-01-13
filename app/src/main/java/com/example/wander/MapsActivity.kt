9babc0b1a4281f2eebdcd419f435f8bb60cb4dd9
Simran Nijhar <simran.nijhar@gmail.com>
2020-01-13 19:31:56 +0800

    tracklocation done

--- app/src/main/java/com/example/wander/MapsActivity.kt
+++ app/src/main/java/com/example/wander/MapsActivity.kt
@@ -1,20 +1,29 @@
 package com.example.wander
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
+import java.util.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
+    private val TAG = MapsActivity::class.java.simpleName
+    private val REQUEST_LOCATION_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,23 +74,72 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        val latitude = 3.058032
-        val longitude = 101.739222
-        val zoomLevel = 15f
+        //val latitude : Double =
+       // val longitude = 101.739222
+       // val zoomLevel = 15f
 
-        val homeLatLng = LatLng(latitude, longitude)
+       // val homeLatLng = LatLng(latitude, longitude)
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-        map.addMarker(MarkerOptions().position(homeLatLng))
+      //  map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        //map.addMarker(MarkerOptions().position(homeLatLng))
 
         setMapLongClick(map)
+        setPoiClick(map)
+        setMapStyle(map)
+        enableMayLocation()
     }
 
     private fun setMapLongClick(map:GoogleMap){
         map.setOnMapLongClickListener { latLng ->
+            val snippet = String.format(Locale.getDefault(),
+                "Lat : %1$.5f, Long : %2$.5f", latLng.latitude, latLng.longitude)
             map.addMarker(
-                MarkerOptions().position(latLng)
+                MarkerOptions().position(latLng).title(getString(R.string.dropped_pin)).snippet(snippet).icon(
+                    BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+                )
             )
+        }
+    }
+
+    private fun setPoiClick(map:GoogleMap){
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(MarkerOptions().position(poi.latLng).title(poi.name))
+            poiMarker.showInfoWindow()
+        }
+    }
+
+    private fun setMapStyle(map:GoogleMap){
+        try{
+            val success = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+
+            if(!success){
+                Log.e(TAG, "Style parsing failed")
+            }
+        }catch (e: Resources.NotFoundException){
+            Log.e(TAG, "Can't find style. Error : ", e)
+        }
+    }
+
+    private fun isPermissionGranted() : Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun enableMayLocation(){
+        if(isPermissionGranted()){
+            map.isMyLocationEnabled = true
+        }
+        else{
+            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        if(requestCode == REQUEST_LOCATION_PERMISSION){
+            if(grantResults.contains(PackageManager.PERMISSION_GRANTED))
+                enableMayLocation()
         }
     }
 }
