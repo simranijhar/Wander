031651451cb3c2154174c23b0dc1aa3351bb059c
Simran Nijhar <simran.nijhar@gmail.com>
2020-01-15 14:55:39 +0800

    fuck u

--- app/src/main/java/com/example/wander/MapsActivity.kt
+++ app/src/main/java/com/example/wander/MapsActivity.kt
@@ -22,8 +22,8 @@ import java.util.*
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
-    private val TAG = MapsActivity::class.java.simpleName
-    private val REQUEST_LOCATION_PERMISSION = 1
+//    private val TAG = MapsActivity::class.java.simpleName
+//    private val REQUEST_LOCATION_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,34 +32,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.map_options, menu)
-        return true
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        val inflater = menuInflater
+//        inflater.inflate(R.menu.map_options, menu)
+//        return true
+//    }
 
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-        // Change the map type based on the user's selection.
-        R.id.normal_map -> {
-            map.mapType = GoogleMap.MAP_TYPE_NORMAL
-            true
-        }
-        R.id.hybrid_map -> {
-            map.mapType = GoogleMap.MAP_TYPE_HYBRID
-            true
-        }
-        R.id.satellite_map -> {
-            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
-            true
-        }
-        R.id.terrain_map -> {
-            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
-            true
-        }
-        else -> super.onOptionsItemSelected(item)
-    }
+//    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+//        // Change the map type based on the user's selection.
+//        R.id.normal_map -> {
+//            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+//            true
+//        }
+//        R.id.hybrid_map -> {
+//            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+//            true
+//        }
+//        R.id.satellite_map -> {
+//            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+//            true
+//        }
+//        R.id.terrain_map -> {
+//            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+//            true
+//        }
+//        else -> super.onOptionsItemSelected(item)
+//    }
 
     /**
      * Manipulates the map once available.
@@ -74,72 +75,71 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        //val latitude : Double =
-       // val longitude = 101.739222
-       // val zoomLevel = 15f
-
-       // val homeLatLng = LatLng(latitude, longitude)
-
-      //  map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
-        //map.addMarker(MarkerOptions().position(homeLatLng))
+//        val latitude = 3.058231
+//        val longitude = 101.739222
+//        val zoomLevel = 15f
+//
+//        val homeLatLng = LatLng(latitude, longitude)
+//
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+//        map.addMarker(MarkerOptions().position(homeLatLng))
 
         setMapLongClick(map)
-        setPoiClick(map)
-        setMapStyle(map)
-        enableMayLocation()
+      //  setPoiClick(map)
+      //  setMapStyle(map)
+      //  enableMyLocation()
     }
 
     private fun setMapLongClick(map:GoogleMap){
         map.setOnMapLongClickListener { latLng ->
-            val snippet = String.format(Locale.getDefault(),
-                "Lat : %1$.5f, Long : %2$.5f", latLng.latitude, latLng.longitude)
+            val snippet = String.format(Locale.getDefault(),"Lat : %1$.5f, Long : %2$.5f", latLng.latitude, latLng.longitude)
             map.addMarker(
                 MarkerOptions().position(latLng).title(getString(R.string.dropped_pin)).snippet(snippet).icon(
-                    BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)
+                    BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)
                 )
             )
         }
     }
 
-    private fun setPoiClick(map:GoogleMap){
-        map.setOnPoiClickListener { poi ->
-            val poiMarker = map.addMarker(MarkerOptions().position(poi.latLng).title(poi.name))
-            poiMarker.showInfoWindow()
-        }
-    }
-
-    private fun setMapStyle(map:GoogleMap){
-        try{
-            val success = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
-
-            if(!success){
-                Log.e(TAG, "Style parsing failed")
-            }
-        }catch (e: Resources.NotFoundException){
-            Log.e(TAG, "Can't find style. Error : ", e)
-        }
-    }
-
-    private fun isPermissionGranted() : Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun enableMayLocation(){
-        if(isPermissionGranted()){
-            map.isMyLocationEnabled = true
-        }
-        else{
-            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray) {
-        if(requestCode == REQUEST_LOCATION_PERMISSION){
-            if(grantResults.contains(PackageManager.PERMISSION_GRANTED))
-                enableMayLocation()
-        }
-    }
+//    private fun setPoiClick(map:GoogleMap){
+//        map.setOnPoiClickListener { poi ->
+//            val poiMarker = map.addMarker(MarkerOptions().position(poi.latLng).title(poi.name))
+//            poiMarker.showInfoWindow()
+//        }
+//    }
+//
+//    private fun setMapStyle(map:GoogleMap){
+//        try{
+//            val success = map.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+//
+//            if(!success){
+//                Log.e(TAG, "Style parsing failed")
+//            }
+//        }catch (e: Resources.NotFoundException){
+//            Log.e(TAG, "Can't find style. Error : ", e)
+//        }
+//    }
+//
+//    private fun isPermissionGranted() : Boolean {
+//        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+//    }
+//
+//    private fun enableMyLocation(){
+//        if(isPermissionGranted()){
+//            map.isMyLocationEnabled = true
+//        }
+//        else{
+//            ActivityCompat.requestPermissions(this, arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+//        }
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<String>,
+//        grantResults: IntArray) {
+//        if(requestCode == REQUEST_LOCATION_PERMISSION){
+//            if(grantResults.contains(PackageManager.PERMISSION_GRANTED))
+//                enableMyLocation()
+//        }
+//    }
 }
